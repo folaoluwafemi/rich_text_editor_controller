a7e97a9a795bb3bfbb8b8045d68e6df830713baf
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-04-08 20:45:22 +0100

    add TextDecorationEnum to exports

--- lib/rich_text_editor_controller.dart
+++ lib/rich_text_editor_controller.dart
@@ -1,6 +1,11 @@
 library rich_text_editor_controller;
 
 export 'package:rich_text_editor_controller/src/text_editor/text_editor_barrel.dart'
-    show TextMetadata, TextDeltas, TextDelta, RichTextEditorController;
+    show
+        TextMetadata,
+        TextDeltas,
+        TextDelta,
+        RichTextEditorController,
+        TextDecorationEnum;
 export 'package:rich_text_editor_controller/src/text_editor/ui/widgets/rich_text_field.dart'
     show RichTextField;
