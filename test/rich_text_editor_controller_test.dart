0ddf39777add3acc4b652c1c334af15e441e32bd
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-05-22 17:53:25 +0100

    fix issue

--- test/rich_text_editor_controller_test.dart
+++ test/rich_text_editor_controller_test.dart
@@ -1,13 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group(
-    'rich_text_editor_controller_test',
-    () {
-      test(
-        '',
-        () async {},
-      );
-    },
-  );
-}
+void main() {}
